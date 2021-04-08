9a45aa09823494c54d938c80317f769e534b74df
dmalchowLFC <80545257+dmalchowLFC@users.noreply.github.com>
2021-04-08 15:49:11 -0700

    Second Commit
    Got the majority of the functionality completed. Need to still update code so that input boxes are cleared once form is submitted and add some styling.

--- app.js
+++ app.js
@@ -1,1 +1,32 @@
-x = 0
+document.getElementById("submitButton").addEventListener("click", append);
+const tbodyElement = document.querySelector("tbody");
+const formElement = document.querySelector("form");
+
+function append(e) {
+    e.preventDefault();
+
+    const payment = document.getElementById("currency").value;
+    const info = document.getElementById("description").value;
+    const date = document.getElementById("date").value;
+    const amount = document.getElementById("cost").value;
+    const newRow = `
+    <tr>
+        <td>${payment}</td>
+        <td>${info}</td>
+        <td>${date}</td>
+        <td>${amount}</td>
+        <td><button class="deleteBtn">Delete</button></td>
+    </tr>`;
+
+    tbodyElement.innerHTML += newRow;
+};
+
+function deleteRow(e) {
+    if (!e.target.classList.contains('deleteBtn')) {
+        return;
+    }
+    const btn = e.target;
+    btn.closest('tr').remove();
+};
+
+tbodyElement.addEventListener("click", deleteRow);
